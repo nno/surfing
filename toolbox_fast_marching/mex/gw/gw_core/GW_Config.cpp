81e1cc09ae08990df306512b47ba3de5af088653
Nikolaas N. Oosterhof <nikolaas.oosterhof@unitn.it>
2015-04-24 16:26:35 +0200

    ENH: use "fputs" instead of the unsafe "fprintf" when printing a string

--- toolbox_fast_marching/mex/gw/gw_core/GW_Config.cpp
+++ toolbox_fast_marching/mex/gw/gw_core/GW_Config.cpp
@@ -26,7 +26,12 @@ void GW::GW_OutputComment( const char* str )
 	if( GW_OutputStream!=NULL )
 	{
 		string str2 = gw_alinea + str + gw_endl;
-		fprintf( GW_OutputStream, str2.c_str() );
+        //The line below (which was the original) is considered unsafe
+        //when compiling under Ubuntu with default options.
+        //Instead, fputs is used.
+        //
+		//fprintf( GW_OutputStream, str2.c_str() );
+        fputs(str2.c_str(), GW_OutputStream);
 	}
 }
 
